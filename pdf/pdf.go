d3218247692a340f7b64a0add4fef703b613527a
evellynquanto <evellyn.costa@contaquanto.com.br>
2022-03-31 12:36:36 -0300

    feature(PDF): splitting pdf ins parts

--- pdf/pdf.go
+++ pdf/pdf.go
@@ -3,63 +3,123 @@ package pdf
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 
 	pdfcpu "github.com/pdfcpu/pdfcpu/pkg/api"
 )
 
-const dir = "dir"
+const dir = "tmp/"
 const maximumFileSize = 47185920
+const fileExtension = ".pdf"
 
-func SplitPDF(path string) error {
-	file, err := os.Open(path)
+func SplitPDF(fileName string) error {
+	os.Chdir(dir)
+	file, err := os.Open(fileName)
 
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
 
-	err = pdfcpu.Split(file, "", path, 1, nil)
+	err = pdfcpu.Split(file, "", fileName, 1, nil)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
+
+	totalPages, err := pdfcpu.PageCount(file, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if err != nil {
+		fmt.Println("Err", err)
+		return err
+	}
+	currentPage := 1
+
+	for i := 1; i < totalPages; i++ {
+		partsNames, lastPage, err := extractPartsNames(currentPage, totalPages, fileName, i)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+		mergePages(partsNames, fileName, i)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		currentPage = lastPage
+
+		isLastPage, err := isLastPage(partsNames, totalPages)
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		if isLastPage {
+			break
+		}
+
+	}
+
+	return nil
+}
+
+func extractPartsNames(currentPage int, totalPages int, fileName string, partNumber int) ([]string, int, error) {
 	inFiles := []string{}
-	pages, err := pdfcpu.PageCount(file, nil)
-
-	if err != nil {
-		fmt.Println(err)
-		return err
-	}
-
-	fileName := "_"
-	fileExtension := ".pdf"
+	completeFileName := "_"
 	totalSize := int64(0)
+	for i := currentPage; i <= totalPages; i++ {
+		completeFileName = strings.Trim(fileName, fileExtension) + "" + "_" + strconv.Itoa(i) + fileExtension
 
-	for i := 1; i <= pages; i++ {
-		fileName = strings.Trim(path, fileExtension) + "" + "_" + strconv.Itoa(i) + fileExtension
-
-		fi, err := os.Stat(fileName)
+		fi, err := os.Stat(completeFileName)
 		if err != nil {
 			fmt.Println("Err", err)
-			return err
+			return nil, 0, err
 		}
 		size := fi.Size()
 
 		totalSize = size + totalSize
-
+		currentPage = i
 		if totalSize > maximumFileSize {
-			fmt.Println("page: ", i-1)
-			fmt.Println(totalSize)
 			break
 		}
 
-		inFiles = append(inFiles, fileName)
+		inFiles = append(inFiles, completeFileName)
+	}
+	partNumber++
 
+	return inFiles, currentPage, nil
+}
+
+func mergePages(parts []string, fileName string, partNumber int) error {
+	fileName = strings.Trim(fileName, fileExtension)
+	fileName = fileName + "part" + strconv.Itoa(partNumber) + fileExtension
+	err := pdfcpu.MergeCreateFile(parts, fileName, nil)
+	if err != nil {
+		return err
 	}
 
-	fmt.Println(inFiles)
-
 	return nil
+}
+
+func isLastPage(parts []string, totalPage int) (bool, error) {
+	lastPart := parts[len(parts)-1]
+	re := regexp.MustCompile("[0-9]+")
+	page := re.FindAllString(lastPart, -1)
+	pageConverted, err := strconv.Atoi(page[0])
+	if err != nil {
+		return false, err
+	}
+	if pageConverted == totalPage {
+		return true, nil
+	}
+
+	return false, nil
 }
